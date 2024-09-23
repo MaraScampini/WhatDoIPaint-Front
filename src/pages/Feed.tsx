b16fc917219a949e527e7e941faae1826dd0e2c7
Mara Scampini <109754827+MaraScampini@users.noreply.github.com>
2024-09-23 16:47:19 +0200

    Add styles and priority to cards

--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -57,13 +57,22 @@ const Feed = () => {
                             className='w-1/4 aspect-square bg-cover bg-center rounded-xl relative'
                             style={{ backgroundImage: `url(${project.image})` }}
                         >
+                            {project.isPriority && (
+                                <div className='text-lightTeal absolute right-3 top-3'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                                        <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clipRule="evenodd" />
+                                    </svg>
+                                </div>
+
+                            )}
+
                             <div className='absolute bottom-0 left-0 bg-gradient-to-t from-black to-transparent w-full h-1/6 flex items-end justify-between rounded-xl'>
                                 <p className='text-offWhite font-display ps-3 pb-1' >{project.name}</p>
                                 <button
                                     onClick={handleAddUpdate}
                                     className='text-lightTeal pb-1 pe-1'>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                                     </svg>
 
                                 </button>
