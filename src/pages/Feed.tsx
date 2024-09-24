3778a345334041e727fe0f7c06648b25d143f45f
Mara Scampini <109754827+MaraScampini@users.noreply.github.com>
2024-09-24 10:38:53 +0200

    Toggle priority integration in progress, done without error handling

--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import useUserStore from '../store/useUserStore'
 import { useEffect, useState } from 'react';
-import { getProjectsByUser } from '../services/projectService';
+import { getProjectsByUser, togglePriority } from '../services/projectService';
 
 type Project = {
     id: number,
@@ -11,6 +11,7 @@ type Project = {
 };
 
 const Feed = () => {
+    const token = localStorage.getItem('authToken');
     const user = useUserStore((state) => state.user);
     const logout = useUserStore((state) => state.logout);
     const fetchUser = useUserStore((state) => state.fetchUser);
@@ -19,7 +20,6 @@ const Feed = () => {
     const [userProjects, setUserProjects] = useState<Project[]>([]);
 
     useEffect(() => {
-        const token = localStorage.getItem('authToken');
         const loadUser = async () => {
             if (token) {
                 if (!user) {
@@ -45,9 +45,18 @@ const Feed = () => {
         console.log('Painted today!')
     }
 
-    const togglePriority = () => {
+    const handleTogglePriority = async (projectId: number) => {
         // TODO
         console.log('Toggle priority')
+        try {
+            await togglePriority(token!, projectId);
+            const userProjects = await getProjectsByUser(token!);
+            setUserProjects(userProjects);
+        } catch (error) {
+            if (error instanceof Error) {
+
+            }
+        }
     }
 
     return (
@@ -59,10 +68,10 @@ const Feed = () => {
                     {userProjects.map((project) => (
                         <div
                             key={project.id}
-                            className='w-1/4 aspect-square bg-cover bg-center rounded-xl relative transition-all duration-300 ease-in-out hover:outline hover:outline-lightTeal hover:outline-1 hover:cursor-pointer'
+                            className='w-1/6 aspect-square bg-cover bg-center rounded-xl relative transition-all duration-300 ease-in-out hover:outline hover:outline-lightTeal hover:outline-1 hover:cursor-pointer'
                             style={{ backgroundImage: `url(${project.image})` }}
                         >
-                            <button onClick={togglePriority} className='text-lightTeal absolute right-3 top-3'>
+                            <button onClick={() => handleTogglePriority(project.id)} className='text-lightTeal absolute right-3 top-3'>
                                 {project.priority ? (
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 transitition-all ease-in-out duration-300 hover:fill-none hover:stroke-lightTeal hover:stroke-1">
                                         <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clipRule="evenodd" />
--- src/services/projectService.ts
+++ src/services/projectService.ts
@@ -2,15 +2,31 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_API_PROD_URL;
 
+const headers = (token: string) => ({ headers: {
+    Authorization: `Bearer ${token}`
+}});
+
 // GET PROJECTS BY USER
 
 export const getProjectsByUser = async (token: string) => {
     try {
-        let res = await axios.get(`${API_URL}/api/project`, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        });
+        let res = await axios.get(`${API_URL}/api/project`, headers(token));
+        return res.data;
+    } catch (error) {
+        if(axios.isAxiosError(error)) {
+            const errorMessage = error.response?.data;
+            throw new Error(errorMessage);
+        } else {
+            throw new Error('Unknown error');
+        }
+    }
+}
+
+// TOGGLE PROJECT PRIORITY
+
+export const togglePriority = async (token: string, projectId: number) => {
+    try {
+        let res = await axios.put(`${API_URL}/api/project/toggle/${projectId}`, {}, headers(token));
         return res.data;
     } catch (error) {
         if(axios.isAxiosError(error)) {
