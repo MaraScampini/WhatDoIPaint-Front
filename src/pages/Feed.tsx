31d8c19498c995067dd1cd7cd346bd33715bc17b
Mara Scampini <109754827+MaraScampini@users.noreply.github.com>
2024-10-14 10:01:29 +0200

    Paddings

--- src/pages/Feed.tsx
+++ src/pages/Feed.tsx
@@ -118,7 +118,7 @@ const Feed = () => {
                             <div className='absolute bottom-0 left-0 bg-gradient-to-t from-black to-transparent w-full h-1/6 flex items-end justify-between rounded-xl'>
                                 <p className='text-offWhite font-display ps-3 pb-1' >{project.name}</p>
                                 {project.updatedToday ? (
-                                    <div className='text-lightTeal pb-1 pe-1'>
+                                    <div className='text-lightTeal pb-2 pe-2'>
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                                             <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clipRule="evenodd" />
                                         </svg>
@@ -127,7 +127,7 @@ const Feed = () => {
                                 ) : (
                                     <button
                                         onClick={(e) => handleAddUpdate(e, project.id)}
-                                        className='text-lightTeal pb-1 pe-1 group'>
+                                        className='text-lightTeal pb-2 pe-2 group'>
                                         <svg xmlns="http://www.w3.org/2000/svg"
                                             fill="none"
                                             viewBox="0 0 24 24"
