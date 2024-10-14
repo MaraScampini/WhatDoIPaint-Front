f22f9189b717602efd08ca1c1003e241decb92d5
Mara Scampini <109754827+MaraScampini@users.noreply.github.com>
2024-10-14 11:34:53 +0200

    Create update with images only¡

--- src/components/AddUpdatePopup.tsx
+++ src/components/AddUpdatePopup.tsx
@@ -1,4 +1,10 @@
+import { useEffect, useRef, useState } from "react";
+import useFormValidation from "../hooks/useFormValidation";
 import Button from "./Button";
+import { createUpdate } from "../services/updateService";
+import { useNavigate } from "react-router-dom";
+import Loader from "./Loader";
+import useErrorStore from "../store/useErrorStore";
 
 interface ModalProps {
     isOpen: boolean,
@@ -6,28 +12,98 @@ interface ModalProps {
     projectId: number
 }
 
+interface UpdateImagesData {
+    projectId: number,
+    images: Array<string>
+}
+
 const AddUpdatePopup: React.FC<ModalProps> = ({ isOpen, onClose, projectId }) => {
 
     if (!isOpen) return null;
 
+    const [isLoading, setIsLoading] = useState(false);
+
+    const initialImageData: UpdateImagesData = {
+        projectId: projectId,
+        images: []
+    };
+
+    const navigate = useNavigate();
+    const setError = useErrorStore((state) => state.setError);
+
+    const { formValues, handleMultipleFilesSelect } = useFormValidation(initialImageData);
+
+    const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+    const handleClick = () => {
+        if (fileInputRef.current) {
+            fileInputRef.current.click();
+            fileInputRef.current.value = "";
+        }
+    };
+
+    useEffect(() => {
+        const sendImages = async () => {
+            if (formValues.images.length > 0) {
+                setIsLoading(true);
+                try {
+                    await createUpdate(formValues);
+                    navigate(`/project/${projectId}`);
+                } catch (error) {
+                    if(error instanceof Error) {
+                        setError(error.message);
+                    } 
+                } finally {
+                    setIsLoading(false);
+                }
+            }
+        }
+
+        sendImages();
+
+        return () => {
+            setIsLoading(false);
+        };
+        
+    }, [formValues])
+
     return (
         <div className="bg-darkBg bg-opacity-75 fixed inset-0 flex items-center justify-center z-40">
-            <div className="w-1/2 h-2/5 bg-darkGrey rounded-md
-            flex flex-col items-center justify-center 
-            text-offWhite font-display uppercase text-3xl 
-            relative
-            ">
-                <div className="absolute top-5 right-5 text-lightTeal hover:text-offWhite hover:cursor-pointer"
-                onClick={onClose}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                </svg>
+            {isLoading ? (
+                <div>
+                    <Loader/>
                 </div>
-                <p>congratulations</p>
-                <p className="mb-5">you are on a <span className="text-lightTeal">X</span> day streak!</p>
-                <Button text="add more info" buttonType="button" />
-                <Button text="add pictures" buttonType="button" />
-            </div>
+            ) : (
+                <div className="w-1/2 h-2/5 bg-darkGrey rounded-md
+                                flex flex-col items-center justify-center 
+                                 text-offWhite font-display uppercase text-3xl 
+                                relative
+                ">
+                    <div className="absolute top-5 right-5 text-lightTeal hover:text-offWhite hover:cursor-pointer"
+                        onClick={onClose}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-8">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                        </svg>
+                    </div>
+                    <p>congratulations</p>
+                    <p className="mb-5">you are on a <span className="text-lightTeal">X</span> day streak!</p>
+                    <Button text="add more info" buttonType="button" />
+                    <div
+                        onClick={handleClick}>
+                        <Button text="add pictures" buttonType="button" />
+                        <input
+                            type="file"
+                            accept="image/"
+                            multiple
+                            className="hidden"
+                            ref={fileInputRef}
+                            onChange={handleMultipleFilesSelect('images')
+                            }
+                        />
+                    </div>
+                </div>
+            )
+            }
         </div>
     )
 }
--- src/services/updateService.ts
+++ src/services/updateService.ts
@@ -32,3 +32,28 @@ export const createShortUpdate = async (projectId: number) => {
         }
     }
 }
+
+// CREATE NEW UPDATE
+
+interface UpdateData {
+    projectId: number,
+    images?: Array<string>,
+    title?: string,
+    description?: string,
+    elements?: Array<number>,
+    squads?: Array<number>
+}
+
+export const createUpdate = async (updateData: UpdateData) => {
+    try {
+        let res = await apiClient.post(`/api/update`, updateData);
+        return res.data;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            const errorMessage = error.response?.data;
+            throw new Error(errorMessage);
+        } else {
+            throw new Error('Unknown error');
+        }
+    }
+}
