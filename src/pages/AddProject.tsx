bf4dd41a5f3855a80dfab398bc5a542b4f94c731
Mara Scampini <109754827+MaraScampini@users.noreply.github.com>
2024-10-16 15:30:58 +0200

    Back from add project

--- src/pages/AddProject.tsx
+++ src/pages/AddProject.tsx
@@ -110,7 +110,14 @@ const AddProject = () => {
 
     return (
         <div className="text-offWhite flex flex-col ms-10 ml-10">
-            <div className="font-display text-3xl font-semibold">New Project</div>
+            <div className="flex gap-x-3 items-center">
+                <div onClick={() => navigate(`/feed`)} className="hover:text-lightTeal transition-all duration-100 hover:cursor-pointer">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                    </svg>
+                </div>
+                <div className="font-display text-3xl font-semibold">New Project</div>
+            </div>
             <form onSubmit={handleSubmitProject} className="w-full flex flex-col">
                 <div className='w-full flex flex-col'>
                     <div className='flex w-full justify-between'>
@@ -136,32 +143,32 @@ const AddProject = () => {
                             </div>
                         </div>
                         <div className='flex flex-col w-2/5 me-10'>
-                                <p className="font-display text-lightTeal uppercase font-light">upload cover image</p>
-                                <div
-                                    className="mt-4 p-6 border-dashed border-2 border-lightTeal rounded-lg cursor-pointer h-80 flex items-center justify-center text-lightTeal hover:border-solid hover:text-offWhite"
-                                    onDrop={handleImageDrop('image')}
-                                    onDragOver={handleDragOver}
-                                    onClick={handleClick}
-                                >
-                                    {formValues.image ? (
-                                        <div className="mt-4">
-                                            <img src={formValues.image} alt="Preview" className="max-h-40 object-contain" onClick={(e) => handleDeleteImage(e)} />
-                                        </div>
+                            <p className="font-display text-lightTeal uppercase font-light">upload cover image</p>
+                            <div
+                                className="mt-4 p-6 border-dashed border-2 border-lightTeal rounded-lg cursor-pointer h-80 flex items-center justify-center text-lightTeal hover:border-solid hover:text-offWhite"
+                                onDrop={handleImageDrop('image')}
+                                onDragOver={handleDragOver}
+                                onClick={handleClick}
+                            >
+                                {formValues.image ? (
+                                    <div className="mt-4">
+                                        <img src={formValues.image} alt="Preview" className="max-h-40 object-contain" onClick={(e) => handleDeleteImage(e)} />
+                                    </div>
 
-                                    ) : (
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="size-24">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15" />
-                                        </svg>
+                                ) : (
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1} stroke="currentColor" className="size-24">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15" />
+                                    </svg>
 
-                                    )}
-                                </div>
-                                <input
-                                    type="file"
-                                    accept="image/*"
-                                    ref={fileInputRef}
-                                    style={{ display: 'none' }}
-                                    onChange={handleFileSelect('image')}
-                                />
+                                )}
+                            </div>
+                            <input
+                                type="file"
+                                accept="image/*"
+                                ref={fileInputRef}
+                                style={{ display: 'none' }}
+                                onChange={handleFileSelect('image')}
+                            />
 
                         </div>
                     </div>
