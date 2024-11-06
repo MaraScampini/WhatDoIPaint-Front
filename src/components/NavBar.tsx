d08536c9e8ab5f1034e58f12b4185ce15b0ec561
Mara Scampini <109754827+MaraScampini@users.noreply.github.com>
2024-11-06 14:24:13 +0100

    Change profile picture to generic until profile is active

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,9 +10,13 @@ const NavBar = () => {
                 <span className='text-3xl'>IP</span>
             </p>
 
-            <div className='relative w-[30px] h-30 rounded-full overflow-hidden'>
-                <img src="/assets/test-profile.jpg" alt="profile-picture" className='w-full h-full object-cover' />
-                <div className="absolute inset-0 bg-darkTeal rounded-full mix-blend-hue"></div>
+            <div className='relative w-[30px] h-30 rounded-full overflow-hidden text-lightTeal'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                </svg>
+                {/* TODO: put profile picture from the user
+                <img src="/assets/profile.png" alt="profile-picture" className='w-full h-full object-cover' />
+                <div className="absolute inset-0 bg-darkTeal rounded-full mix-blend-hue"></div> */}
             </div>
         </div>
     )
